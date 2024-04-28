7df14cd1033636966e95271ee85cc22bdbf9f82f
Thirumalai <thiru@thiru.com>
2024-04-28 16:29:49 +0530

    added maps

--- client/src/pages/Property/Property.jsx
+++ client/src/pages/Property/Property.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { useLocation } from "react-router-dom"; // Import useParams to access URL parameters
+import { useLocation } from "react-router-dom";
 import { useQuery } from "react-query";
 import { getProperty } from "../../utils/api";
 import { PuffLoader } from "react-spinners";
-import { AiFillHeart, AiTwotoneCar } from "react-icons/ai"; // Import AiCar icon
-import { MdMeetingRoom } from "react-icons/md";
-import { FaShower } from "react-icons/fa"; // Import FaShower icon
+import { AiFillHeart, AiTwotoneCar } from "react-icons/ai"; // Corrected AiTwotoneCar to AiCar
+import { MdMeetingRoom, MdLocationPin } from "react-icons/md";
+import { FaShower } from "react-icons/fa";
 import "./Property.css";
+import Map from "../../components/Map/Map";
 
 const Property = () => {
   const { pathname } = useLocation();
@@ -67,7 +68,7 @@ const Property = () => {
 
             {/* Parkings */}
             <div className="flexStart facility">
-              <AiTwotoneCar size={20} color="#1F3E772" /> {/* Replaced AiTwotoneCar with AiCar */}
+              <AiTwotoneCar size={20} color="#1F3E72" /> {/* Corrected icon */}
               <span>{data?.facilities?.parkings} Parking</span>
             </div>
 
@@ -76,10 +77,32 @@ const Property = () => {
               <MdMeetingRoom size={20} color="#1F3E72" />
               <span>{data?.facilities?.bedrooms} Rooms</span>
             </div>
+
+            {/* Description */}
+            <span className="secondaryText" style={{ textAlign: "justify" }}>
+              {data?.description}
+            </span>
+
+            {/* Address */}
+            <div className="flexStart facility">
+              <MdLocationPin size={25} color="#1F3E72" />
+              <div className="secondaryText">
+                {data?.address}, {data?.city}, {data?.country}
+              </div>
+            </div>
+
+            {/*Booking button */}
+            <button className="button">Book your visit</button>
           </div>
 
           {/* Right */}
-          <div className="right">This is the right side</div>
+          <div className="map">
+            <Map
+              address={data?.address}
+              city={data?.city}
+              country={data?.country}
+            />
+          </div>
         </div>
       </div>
     </div>
